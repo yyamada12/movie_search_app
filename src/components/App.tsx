7ca0caf14c4af3b55e1d6f60e5b665b0371b8d14
yyamada12 <12yacropolisy@gmail.com>
2020-03-06 09:22:10 +0900

    add Search

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Search from "./Search";
 import "../App.css";
 
 function App() {
   return (
     <div className="App">
       <Header text="Hooked" />
+      <Search search={searchValue => {}} />
     </div>
   );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+interface IProps {
+  search: (searchValue: string) => void;
+}
+
+const Search = (props: IProps) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchInputChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const resetInputField = () => {
+    setSearchValue("");
+  };
+
+  const callSearchFunction = (
+    e: React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  };
+
+  return (
+    <form className="search">
+      <input
+        value={searchValue}
+        onChange={handleSearchInputChanges}
+        type="text"
+      />
+      <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+    </form>
+  );
+};
+
+export default Search;
