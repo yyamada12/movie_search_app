4cf2774f3c47ba96a9ab9fe1ff95ec8a3d3ae57e
yyamada12 <12yacropolisy@gmail.com>
2020-03-08 23:42:30 +0900

    add API call in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Search from "./Search";
 import Movie from "./Movie";
 import "../App.css";
 
-function App() {
-  const movies = [
-    {
-      Title: "Iron Man",
-      Year: "2008",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg"
-    }
-  ];
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=Harry Potter&apikey=4a3b711b";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = (searchValue: string) => {
+    setLoading(true);
+    setErrorMessage(null);
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+        }
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
       <Header text="Hooked" />
-      <Search search={searchValue => {}} />
+      <Search search={search} />
       <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
-        {movies.map((m, i) => (
-          <Movie key={i} movie={m} />
-        ))}
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((m, i) => <Movie key={i} movie={m} />)
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
